d0f92988d04bb61a3f09b9e8b535a8b6731e337b
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-06 10:12:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -101,33 +101,14 @@ function login () {
 }
 // Validate Functions
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/
-  if (expression.test(email) == true) {
-    // Email is good
-    return true
-  } else {
-    // Email is not good
-    return false
-  }
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  return expression.test(email);
 }
 
 function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password < 6) {
-    return false
-  } else {
-    return true
-  }
+  return password.length >= 6;
 }
 
 function validate_field(field) {
-  if (field == null) {
-    return false
-  }
-
-  if (field.length <= 0) {
-    return false
-  } else {
-    return true
-  }
+  return field !== null && field.length > 0;
 }
