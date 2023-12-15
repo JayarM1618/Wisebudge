5f55f4e3644f1d717d3bc2f77a5b8b36f47e7a17
JayarM1618 <151903474+JayarM1618@users.noreply.github.com>
2023-12-15 21:11:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase, ref, set, update } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 // Your web app's Firebase configuration
   const firebaseConfig = {
